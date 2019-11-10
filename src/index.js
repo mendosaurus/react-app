9e36ce265f94e04e3b5d9dfe268712c6a1b7886b
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-10 15:38:33 -0500

    add helper function, and return winner

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice(); //copy original array
+    if(calculateWinner(squares)||squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
@@ -42,8 +45,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player ${this.state.xIsNext ? "X" : "O"}`;
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    }
+    else{
+      status = `Next player ${this.state.xIsNext ? "X" : "O"}`;
+    }
     return (
       <div>
         <div className="status"> {status} </div>
@@ -80,3 +89,23 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root")); // render Game component in root
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
